6bd48606bb1ab4122d385f6ed2e67a74f75a55df
kurs <kurs@kurs-Komputer>
2017-08-20 11:18:17 +0200

    Konfliktowe zmiany w 2.0

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,6 +3,7 @@ package hello;
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("Tutaj wersja 2.0");
 		System.out.println("Pierwsza wersja");
 		System.out.println("Druga wersja");
 		System.out.println("wersja 2.0");
